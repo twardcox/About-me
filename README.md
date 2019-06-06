e44e276d5989825e2e3251889a06f11159d3e545
Jane Hur <janehur8@gmail.com>
2019-06-05 23:50:29 -0700

    Updated for Lab 3

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # About-me
-Lab 02: About Me
+Lab 03: About Me
+
+Added enhancements: 
+
+- Used a multidimensional array to store the questions and answers for the first 5 questions.
+- Added a 6th question which gives the user 4 tries to get the answer correct
+- Added a 7th question which gives the user 6 tries to get the answer correct
+- Displays the user's name and quiz score on the page
--- js/app.js
+++ js/app.js
@@ -1,71 +1,98 @@
 'use strict';
+var numberCorrect = 0;
 
-//WHERE WAS I BORN
+var userName = prompt('What is your name?');
 
-var bornSeattle = prompt('Was I born in Seattle?');
+alert('Welcome ' + userName + '. Answer the following questions with a "Yes" or "No" to learn a little bit about me.');
 
-if (bornSeattle.toLowerCase() === 'yes') {
-  alert ('Nope! I was born in Chicago.');
+// Multi-dimensional array to store questions and answers
+
+var questionAnswers = [
+  ['Was Jane born in Seattle?', 'n', 'I was born in Chicago!'],
+  ['Does Jane love being a mom of two energetic boys?', 'y', 'Life with two young boys is tiring, but I wouldn\'t have it any other way'],
+  ['Does Jane get enough sleep?', 'n', 'What is sleep????'],
+  ['Does Jane miss her family while she is at CodeFellows?', 'y', 'I miss my family, but they are the reason why I am here.'],
+  ['Is Jane a software developer', 'y', 'I AM a software developer...in training.']
+];
+
+
+var numberOfQuestions = questionAnswers.length;
+
+// Loop to go through all the questions in the questionAnswer array
+
+for (var i=0; i<numberOfQuestions; i++){
+  var answer = prompt (questionAnswers[i][0]);
+  var userAnswer = answer.toLowerCase();
+
+  // Checks if the user's answer is correct or not
+
+  if (userAnswer.charAt(0) === questionAnswers[i][1]){
+    alert ('Correct! '+ questionAnswers[i][2]);
+    console.log('Question '+ (i+1) + ': correct');
+    numberCorrect += 1;
+  } else {
+    alert ('Incorrect! ' + questionAnswers[i][2]);
+    console.log('Question '+ (i+1) + ': incorrect');
+  }
+  
 }
-else if (bornSeattle.toLowerCase() === 'no') {
-  alert ('How\'d you know? I was born in Chicago, not Seattle!');
+
+
+
+
+
+//Pick a random number as the favorite number
+var favNumber = Math.floor(Math.random() * 10) + 1;
+//console.log('Favorite Number: '+favNumber+typeof(favNumber));
+
+//Four tries to guess the correct number
+for (var j=0; j<4; j++){
+  var userGuess = parseInt(prompt ('Guess Jane\'s favorite number from 1 to 10'));
+  //console.log('j:'+ j + 'guess: '+userGuess);
+
+  if (j===3){
+    tries = 'You\'re out of tries. Let\'s move on.';
+    console.log('Question 6: incorrect');
+  } else {
+    var tries = 'You have ' + (3-j) + ' more tries.';
+  }
+
+  if (userGuess < favNumber){
+    alert('Too low. '+tries);
+  } else if (userGuess > favNumber){
+    alert('Too high. '+tries);
+  } else if (userGuess === favNumber){
+    alert('Correct!');
+    console.log('Question 6: correct');
+    numberCorrect += 1;
+    j=4;
+  }
 }
-else {
-  alert ('I was born in Chicago!');
+
+var statesLived = ['IL', 'GA'];
+
+for (var k=0; k<6; k++){
+  var userStateGuess = prompt ('Can you guess a state that Jane lived in besides Washington? Use state abbreviations.');
+  //console.log(userStateGuess.toUpperCase());
+
+  if (k===5){
+    triesState = 'You\'re out of tries. Let\'s move on.';
+    console.log('Question 7: incorrect');
+  } else {
+    var triesState = 'You have ' + (5-k) + ' more tries.';
+  }
+
+  //console.log (statesLived[0] + statesLived[1] + userStateGuess.toUpperCase());
+
+  if (userStateGuess.toUpperCase() === statesLived[0] || userStateGuess.toUpperCase() === statesLived[1]){
+    alert('Correct!');
+    console.log('Question 7: correct');
+    numberCorrect += 1;
+    k=6;
+  } else {
+    alert(triesState);
+  }
 }
-console.log('Born in Seattle. Answered: '+bornSeattle.toUpperCase());
 
-
-//ENERGETIC BOYS
-
-var myBoys = prompt ('Do you love being a mom of two energetic boys?');
-
-if (myBoys.toLowerCase() === 'yes') {
-  alert ('Of course! I wish I had thier energy and enthusiasm');
-}
-else {
-  alert ('Life with two young boys is tiring, but I wouldn\'t have it any other way' ); 
-}
-console.log('Energtic boys. Answered: '+ myBoys.toUpperCase());
-
-
-
-//ENOUGH SLEEP
-
-var enoughSleep = prompt ('Do you get enough sleep?');
-
-if (enoughSleep.toLocaleLowerCase === 'yes') {
-  alert ('What is sleep? See previous question');
-}
-else {
-  alert ('Definitely don\'t get enough!');
-}
-console.log('Sleep. Answered: '+ enoughSleep.toUpperCase());
-
-
-
-//MISS FAMILY
-
-var missFamily = prompt ('Do you miss your family while you are at CodeFellows?');
-
-if (missFamily.toLowerCase()==='yes'){
-  alert('Yes! I miss my family, but they are the reason why I am here.');
-}
-else {
-  alert('Of course I miss my family. Hope we can take a nice long vacation when I graduate!');
-}
-console.log('Miss family. Answered: '+ missFamily.toUpperCase());
-
-
-
-//SOFTWARE DEVELOPER
-
-var softwareDev = prompt ('Are you a software developer');
-
-if (softwareDev.toLowerCase() === 'yes') {
-  alert ('That\'s right! I am a software developer...in training.');
-}
-else {
-  alert ('Not technically, but I will be!');
-}
-console.log("Software dev. Answered: " + softwareDev.toUpperCase());
+//Displays a message on the page with the user's name and the number of correct answers
+document.getElementById('correct').innerText = userName + ', you got ' + numberCorrect + ' correct!';
